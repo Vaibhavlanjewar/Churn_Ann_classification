68cda638471d3231f3a1cd700e9b452fb17ba442
Vaibhav Nivrutti Lanjewar <105924348+Vaibhavlanjewar@users.noreply.github.com>
2024-07-29 21:17:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ with open('scaler.pkl', 'rb') as file:
 
 
 ## streamlit app
-st.title('Customer Churn PRediction')
+st.title('Customer Churn Prediction')
 
 # User input
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
